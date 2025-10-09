051fc9a211a4371521ab1d6a84e08c8f08e433b6
script-vn <7dwyfrjtdv@privaterelay.appleid.com>
2025-10-09 14:11:33 +0700

    Update printers.ps1

--- printers.ps1
+++ printers.ps1
@@ -1,4 +1,4 @@
 # Open-DevicesAndPrinters.ps1
 # This script opens the "Devices and Printers" window on Windows 10.
-
+# in CMD can run : control printers or : explorer shell:::{A8A91A66-3A7D-4424-8D24-04E180695C7A}
 Start-Process control.exe printers
