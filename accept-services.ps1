9734ae65a3e09abada33aeb75d65b60b815ee264
script-vn <7dwyfrjtdv@privaterelay.appleid.com>
2025-10-15 16:34:37 +0700

    Update accept-services.ps1

--- accept-services.ps1
+++ accept-services.ps1
@@ -1,4 +1,13 @@
-$sid = Read-Host "input SID User:"
+# $sid = Read-Host "input SID User:"
+$sidList = @(wmic useraccount get name,sid | Select-String "S-1")
+if ($sidList.Count -ge 5) {
+    $line = $sidList[4].Line
+    $sid = ($line -split '\s+')[1]
+#    Write-Output "SID thứ 5 là: $sid"
+} else {
+#    Write-Output "Không có đủ 5 SID trong danh sách."
+}
+
 cd C:\Users\Admin
 do {
     $servicename = Read-Host "Input Service Name or Press Enter to quick:"
