145032c68e77c8b40e76156b06b8c4e60a9e841f
script-vn <7dwyfrjtdv@privaterelay.appleid.com>
2025-10-15 16:17:02 +0700

    Update accept-services.ps1

--- accept-services.ps1
+++ accept-services.ps1
@@ -1,9 +1,10 @@
+$sid = Read-Host "input SID User:"
 cd C:\Users\Admin
 do {
     $servicename = Read-Host "Input Service Name or Press Enter to quick:"
     
     if (![string]::IsNullOrWhiteSpace($servicename)) {
-        $sid = (whoami /user | Select-String -Pattern "S-1.*").Matches.Value
+      #  $sid = (whoami /user | Select-String -Pattern "S-1.*").Matches.Value
         $sdString = "D:(A;;CCLCSWLOCRRC;;;AU)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;RPWPCR;;;{0})" -f $sid
         
         Write-Host "Accepting Services : $servicename"
