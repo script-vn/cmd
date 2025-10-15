6a88bd85fa48d57d157230cec2d84906befe7946
script-vn <7dwyfrjtdv@privaterelay.appleid.com>
2025-10-15 15:03:30 +0700

    Update accept-services.ps1

--- accept-services.ps1
+++ accept-services.ps1
@@ -1,15 +1,15 @@
-
+cd C:\Users\Admin
 do {
-    $servicename = Read-Host "Nhập tên dịch vụ cần chỉnh sửa (hoặc nhấn Enter để thoát)"
+    $servicename = Read-Host "Input Service Name or Press Enter to quick:"
     
     if (![string]::IsNullOrWhiteSpace($servicename)) {
         $sid = (whoami /user | Select-String -Pattern "S-1.*").Matches.Value
         $sdString = "D:(A;;CCLCSWLOCRRC;;;AU)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;RPWPCR;;;{0})" -f $sid
         
-        Write-Host "Đang áp dụng quyền cho dịch vụ: $servicename"
+        Write-Host "Accepting Services : $servicename"
         sc.exe sdset $servicename $sdString
-        Write-Host "Hoàn tất chỉnh sửa quyền cho dịch vụ `"$servicename`".`n"
+        Write-Host "Acceptep Start-Stop Services `"$servicename`".`n"
     }
 } while (![string]::IsNullOrWhiteSpace($servicename))
 
-Write-Host "Đã thoát script."
+Write-Host "Bye!."
