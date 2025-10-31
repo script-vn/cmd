408b360f5ad6903fa5089fdf6f130fc6875fd7f1
script-vn <7dwyfrjtdv@privaterelay.appleid.com>
2025-10-31 10:59:44 +0700

    Update service denial script with Vietnamese messages

--- deny-services.ps1
+++ deny-services.ps1
@@ -1,23 +1,26 @@
 # $sid = Read-Host "input SID User:"
+
 $sidList = @(wmic useraccount get name,sid | Select-String "S-1")
-if ($sidList.Count -ge 5) {
-    $line = $sidList[4].Line
-    $sid = ($line -split '\s+')[1]
-#    Write-Output "SID thứ 5 là: $sid"
-} else {
-#    Write-Output "Không có đủ 5 SID trong danh sách."
+$sidListFiltered = $sidList | Where-Object { $_.Line -match "1002" }
+
+foreach ($entry in $sidListFiltered) {
+    $line = $entry.Line
+    $parts = $line -split '\s+'
+    $name = $parts[0]
+    $sid = $parts[1]
+    Write-Output "HUY Quyen USER: $name - co SID: $sid"
 }
 
 cd C:\Users\Admin
 do {
-    $servicename = Read-Host "Input Service Name or Press Enter to quick:"
+    $servicename = Read-Host "NHAP Service Name or NHAN Enter de THOAT:"
     
     if (![string]::IsNullOrWhiteSpace($servicename)) {
         $sdString = "D:(A;;CCLCSWLOCRRC;;;AU)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;RPWPCR;;;{0})" -f $sid
         $sdString = "D:(D;;RPWPCR;;;{0})(A;;CCLCSWLOCRRC;;;AU)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWRPWPDTLOCRRC;;;SY)" -f $sid
-        Write-Host "Deny Services : $servicename"
+        Write-Host "HUY Quyen Services : $servicename"
         sc.exe sdset $servicename $sdString
-        Write-Host "Deny Start-Stop Services `"$servicename`".`n"
+        Write-Host "HUY Quyen Start-Stop Services `"$servicename`".`n"
     }
 } while (![string]::IsNullOrWhiteSpace($servicename))
 
