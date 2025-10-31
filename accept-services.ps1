4f82245c54433735114608235bff2df4053a3a57
script-vn <7dwyfrjtdv@privaterelay.appleid.com>
2025-10-31 15:32:58 +0700

    Update accept-services.ps1

--- accept-services.ps1
+++ accept-services.ps1
@@ -1,35 +1,42 @@
 # $sid = Read-Host "input SID User:"
 
 # Da chinh sua
+
+
 $sidList = @(wmic useraccount get name,sid | Select-String "S-1")
-$sidListFiltered = $sidList | Where-Object { $_.Line -match "1002" }
+Write-Output "Chon USER can cap quyen:`n"
 
-if ($sidListFiltered.Count -gt 0) {
-    foreach ($entry in $sidListFiltered) {
-        $line = $entry.Line
-        $parts = $line -split '\s+'
-        $name = $parts[0]
-        $sid = $parts[1]
-        Write-Output "Cap Quyen USER: $name - co SID: $sid"
-    }
-} else {
-    Write-Output "Chon USER Can Cap Quyen:"
-    foreach ($entry in $sidList) {
-        $line = $entry.Line
-        $parts = $line -split '\s+'
-        $name = $parts[0]
-        $sid = $parts[1]
-        Write-Output "USER: $name - SID: $sid"
-    }
-
-    $userSID = Read-Host "NHAP SID da COPY"
-    Write-Output "SID can cap quyen: $userSID"
+foreach ($entry in $sidList) {
+    $line = $entry.Line
+    $parts = $line -split '\s+'
+    $name = $parts[0]
+    $sid = $parts[1]
+    Write-Output "USER: $name : $sid"
 }
 
+$sidInput = Read-Host "SID input"
+$userFound = $false
+
+foreach ($entry in $sidList) {
+    $line = $entry.Line
+    $parts = $line -split '\s+'
+    $name = $parts[0]
+    $sid = $parts[1]
+
+    if ($sid -eq $sidInput) {
+        Write-Output "`nXac Nhan: $name : $sid"
+        $userFound = $true
+        break
+    }
+}
+
+if (-not $userFound) {
+    Write-Output "`nSID not found!!"
+}
 
 cd C:\Users\Admin
 do {
-    $servicename = Read-Host "Nhap Ten Services or Enter de Thoat:"
+    $servicename = Read-Host "Nhap Services-Name or Press Enter to exit:"
     
     if (![string]::IsNullOrWhiteSpace($servicename)) {
       #  $sid = (whoami /user | Select-String -Pattern "S-1.*").Matches.Value
@@ -37,7 +44,6 @@ do {
         
         Write-Host "Cap Quyen Services : $servicename"
         sc.exe sdset $servicename $sdString
-        Write-Host "Da Cap Quyen Start-Stop Services `"$servicename`".`n"
     }
 } while (![string]::IsNullOrWhiteSpace($servicename))
 
