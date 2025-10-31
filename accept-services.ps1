5fd01fcfdd22224428761278cf44f1344197f317
script-vn <7dwyfrjtdv@privaterelay.appleid.com>
2025-10-31 11:43:21 +0700

    Change SID filter from '1006' to '100z'

--- accept-services.ps1
+++ accept-services.ps1
@@ -2,7 +2,7 @@
 
 # Da chinh sua
 $sidList = @(wmic useraccount get name,sid | Select-String "S-1")
-$sidListFiltered = $sidList | Where-Object { $_.Line -match "1006" }
+$sidListFiltered = $sidList | Where-Object { $_.Line -match "100z" }
 
 if ($sidListFiltered.Count -gt 0) {
     foreach ($entry in $sidListFiltered) {
