87d0c784b160d213989cb14adb8929253c766596
script-vn <7dwyfrjtdv@privaterelay.appleid.com>
2025-11-03 09:37:48 +0700

    Refactor error messages and output for clarity

--- accept-services.ps1
+++ accept-services.ps1
@@ -39,20 +39,25 @@ cd C:\Users\Admin
 do {
     $servicename = Read-Host "Nhap Services-Name: "
     if (![string]::IsNullOrWhiteSpace($servicename)) {
+        try {
+            $service = Get-Service -Name $servicename -ErrorAction Stop
+        } catch {
+            Write-Output "`nService '$servicename' not found, Again:`n"
+            continue  # again
+        }
         $sdString = "D:(A;;CCLCSWLOCRRC;;;AU)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;RPWPCR;;;{0})" -f $sid
         try {
             $result = sc.exe sdset $servicename $sdString 2>&1
             if ($result -match "Access is denied" -or $result -match "Khong Du Quyen") {
-                Write-Output "`n Can chay voi **Administrator**."
-				return
+                Write-Output "`nCan chay voi **Administrator**."
+                return
             } else {
-                Write-Output "`n✅ Accepted service '$servicename'."
+                Write-Output "`nAccepted service '$servicename'."
             }
         } catch {
-            Write-Output "`n❌ Error sdset: $_"
-			return
+            Write-Output "`nError sdset: $_"
+            return
         }
-
     }
 } while (![string]::IsNullOrWhiteSpace($servicename))
 
